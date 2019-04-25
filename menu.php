102b982976a653a08f07cb38a39f47a6eae3e7ab
Matheus freire <matheusfreire.ti@hotmail.com>
2019-04-24 22:55:12 -0300

    Update menu.php
    teste menu responsivo

--- menu.php
+++ menu.php
@@ -1,50 +1,42 @@
 <div class="site_wrapper">
     <header id="header">
-        <div id="trueHeader">
-            <div class="wrapper">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2 text-center">
-                            <nav class="navbar navbar-default">
-                                <div class="collapse navbar-collapse" id="barraDataToggle">
-                                    <ul class="nav navbar-nav">
-                                        <li><a href="index.php"> Inicio </a></li>
-                                        <li><a href="servicos2.php" >Serviços</a></li>
-                                        <!-- 					                  <li><a href="#" >Vantagens</a></li>																																		 -->
-                                        <li><a href="Contato2.php" >Contato</a></li>
-                                        <li><a href="SobreEmpresa.php">Sobre Mim</a></li>											
-                                    </ul>								
-                                    <ul class="nav navbar-nav">			
-                                        <!-- menu e submenu -->
-                                        <li class="dropdown">
-                                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Suporte<span class="caret"></span></a>
-                                            <ul class="dropdown-menu" role="Suporte">
-                                                <li>
-                                                    <a id="mibew-agent-button" href="http://www.semprebomtecnologia.com.br/mibew/index.php/chat?locale=en&amp;style=default" 
-                                                       target="_blank" onclick="Mibew.Objects.ChatPopups['59862c8ad3524a42'].open();return false;"> Iniciar o chat Agora</a>
-                                                </li>
-                                                <li><a href="http://www.ammyy.com/en/">Ammy Admin</a></li>
-                                                <li><a href="https://www.teamviewer.com/pt/download/windows/">TeamViwer</a></li>	             		 
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                    <ul class="nav navbar-nav">
-                                        <!-- menu e submenu -->
-                                        <li class="dropdown">
-                                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> area Administrativa <span class="caret"></span></a>
-                                            <ul class="dropdown-menu" role="Suporte">
-                                                <li><a href="./admin/index.php">Fazer Login</a></li>
-                                                <li><a href="#">Quero me Cadastrar</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>							         
-                                
-                                </div>
-                            </nav>    
-                        </div>
-                    </div>           
-                </div> 
-            </div>
+        <div class="col-md-8 col-md-offset-2 text-center">
+            <nav class="navbar navbar-default">
+                <div class="collapse navbar-collapse" id="barraDataToggle">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php"> Inicio </a></li>
+                        <li><a href="servicos2.php" >Serviços</a></li>
+                        <!-- 					                  <li><a href="#" >Vantagens</a></li>																																		 -->
+                        <li><a href="Contato2.php" >Contato</a></li>
+                        <li><a href="SobreEmpresa.php">Sobre Mim</a></li>											
+                    </ul>								
+                    <ul class="nav navbar-nav">			
+                        <!-- menu e submenu -->
+                        <li class="dropdown">
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Suporte<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="Suporte">
+                                <li>
+                                    <a id="mibew-agent-button" href="http://www.semprebomtecnologia.com.br/mibew/index.php/chat?locale=en&amp;style=default" 
+                                        target="_blank" onclick="Mibew.Objects.ChatPopups['59862c8ad3524a42'].open();return false;"> Iniciar o chat Agora</a>
+                                </li>
+                                <li><a href="http://www.ammyy.com/en/">Ammy Admin</a></li>
+                                <li><a href="https://www.teamviewer.com/pt/download/windows/">TeamViwer</a></li>	             		 
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav">
+                        <!-- menu e submenu -->
+                        <li class="dropdown">
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> area Administrativa <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="Suporte">
+                                <li><a href="./admin/index.php">Fazer Login</a></li>
+                                <li><a href="#">Quero me Cadastrar</a></li>
+                            </ul>
+                        </li>
+                    </ul>							         
+                
+                </div>
+            </nav>    
         </div>
     </header>
 </div>
