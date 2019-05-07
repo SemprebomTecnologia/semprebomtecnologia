57a03872b09127f8b29f12176847b4d4cdd48001
Matheus freire <matheusfreire.ti@hotmail.com>
2019-05-07 15:14:39 -0300

    login javscript
    adicionado pagina de login com chamada em javscript a funcao de validação de login

---
+++ admin/verificaLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+// Exibir ou esconder erros do PHP
+error_reporting( error_reporting() & ~E_NOTICE );
+@ini_set('display_errors', 1);
+
+
+ echo "<pre>";
+// var_dump($_GET);
+
+if (isset($_POST['username']) && $_POST['username'] == 'admin')  {        
+    session_start();
+    $_SESSION['username'] = $_POST['usernname'];        
+    header('Location: dashboard.html');
+}else{
+    header('Location: index.php');
+}
+
+
+
+
+
+
+
+?>
